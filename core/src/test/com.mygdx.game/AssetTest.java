56b5a38d9b971fdfe0af0ca2e29860821db4eeab
Chloe <git@eolhc.dev>
2023-03-08 18:58:20 +0000

    fix CI

---
+++ core/src/test/com.mygdx.game/AssetTest.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+import com.badlogic.gdx.Gdx;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AssetTest {
+    @Test
+    public void binTest() {
+        assertTrue(true);
+
+    }
+}
